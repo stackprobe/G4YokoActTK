8493fd2c48f90d2f4b90effd4410a9a49830bd1b
stackprobe <stackprobes@gmail.com>
2019-08-29 09:12:32 +0900

    Dream

--- _Release.bat
+++ _Release.bat
@@ -12,7 +12,7 @@ COPY /B ScarletDiamond\ScarletDiamond\bin\Release\DxLibDotNet.dll out
 C:\Factory\Tools\xcp.exe doc out
 C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
 
-C:\Factory\SubTools\zip.exe /G out ScarletDiamond
+C:\Factory\SubTools\zip.exe /PE- /G out ScarletDiamond
 C:\Factory\Tools\summd5.exe /M out
 
 PAUSE
