895f7993659523420f2a20a8a8a1d99c6305eb77
stackprobe <stackprobes@gmail.com>
2019-09-24 02:44:58 +0900

    Pine

--- ScarletDiamond/_copylib.bat
+++ ScarletDiamond/_copylib.bat
@@ -1,2 +1,3 @@
 C:\Factory\SubTools\CopyLib.exe C:\Dev\Fairy\Donut2\Donut2\Donut2\Common ScarletDiamond\Common
+C:\Factory\SubTools\CsProjAdjustCs.exe ScarletDiamond Common
 ROBOCOPY C:\Dev\Fairy\Donut2\Resource\General ..\Resource\General /MIR
