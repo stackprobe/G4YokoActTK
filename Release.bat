0189944461a18c16b4aeaa94a340aa53b261b88e
stackprobe <stackprobes@gmail.com>
2020-08-12 06:24:56 +0900

    Southern Cross

--- Release.bat
+++ Release.bat
@@ -1,6 +1,6 @@
 C:\Factory\Tools\RDMD.exe /RC out
 
-C:\Factory\SubTools\makeDDResourceFile.exe Resource out\Resource.dat Tools\MaskGZData.exe
+C:\Factory\SubTools\makeDDResourceFile.exe C:\Dat\Resource out\Resource.dat C:\Factory\Program\MaskGZDataForDonut3\MaskGZData.exe
 
 C:\Factory\SubTools\CallConfuserCLI.exe ScarletDiamond\ScarletDiamond\bin\Release\ScarletDiamond.exe out\ScarletDiamond.exe
 rem COPY /B ScarletDiamond\ScarletDiamond\bin\Release\ScarletDiamond.exe out
@@ -10,7 +10,7 @@ COPY /B ScarletDiamond\ScarletDiamond\bin\Release\DxLib_x64.dll out
 COPY /B ScarletDiamond\ScarletDiamond\bin\Release\DxLibDotNet.dll out
 
 C:\Factory\Tools\xcp.exe doc out
-C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
+C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut3\doc out
 
 C:\Factory\SubTools\zip.exe /PE- /RVE- /G out ScarletDiamond
 C:\Factory\Tools\summd5.exe /M out
