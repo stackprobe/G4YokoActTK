858527aae7ce1100b53dc82999e6c621dd0eccf0
stackprobe <stackprobes@gmail.com>
2020-08-12 20:01:38 +0900

    Fortune

--- Release.bat
+++ Release.bat
@@ -1,6 +1,11 @@
 C:\Factory\Tools\RDMD.exe /RC out
 
-C:\Factory\SubTools\makeDDResourceFile.exe C:\Dat\Resource out\Resource.dat C:\Factory\Program\MaskGZDataForDonut3\MaskGZData.exe
+C:\Factory\SubTools\makeDDResourceFile.exe ^
+	C:\Dat\Resource ^
+	/SD Fairy\Donut3\General ^
+	/SD Etoile\ScarletDiamond ^
+	out\Resource.dat ^
+	C:\Factory\Program\MaskGZDataForDonut3\MaskGZData.exe
 
 C:\Factory\SubTools\CallConfuserCLI.exe ScarletDiamond\ScarletDiamond\bin\Release\ScarletDiamond.exe out\ScarletDiamond.exe
 rem COPY /B ScarletDiamond\ScarletDiamond\bin\Release\ScarletDiamond.exe out
