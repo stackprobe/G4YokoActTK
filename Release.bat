8a641ce10c9d7a19e88e14238667e4826194e287
stackprobe <stackprobes@gmail.com>
2020-08-14 17:41:38 +0900

    Mirage

--- Release.bat
+++ Release.bat
@@ -16,6 +16,7 @@ COPY /B G4YokoActTK\G4YokoActTK\bin\Release\DxLibDotNet.dll out
 
 C:\Factory\Tools\xcp.exe doc out
 C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut3\doc out
+COPY /B AUTHORS out
 
 C:\Factory\SubTools\zip.exe /PE- /RVE- /G out G4YokoActTK
 C:\Factory\Tools\summd5.exe /M out
